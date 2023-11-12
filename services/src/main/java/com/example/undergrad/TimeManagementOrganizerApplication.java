1b4fb2faa936f9bfe75b43b4a59e81ee31df7c3c
bpsha <bps0075>
2023-11-11 23:41:22 -0600

    Added in sample data to use, which could be a good idea to show how the data would be used and represented.

--- services/src/main/java/com/example/undergrad/TimeManagementOrganizerApplication.java
+++ services/src/main/java/com/example/undergrad/TimeManagementOrganizerApplication.java
@@ -4,14 +4,69 @@ import com.example.undergrad.model.Events;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
 import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
 
 @SpringBootApplication
 public class TimeManagementOrganizerApplication {
 
+	private static LocalTime startTime = null;
+	private static LocalTime endTime = null;
+	private static LocalDate plannedDate = null;
+	private static String name = null;
+	private static String location = null;
+	private static String scheduleType = null;
+
 	public static void main(String[] args) {
 		SpringApplication.run(TimeManagementOrganizerApplication.class, args);
 
+		//Below is tested data that can be used for showing how the event/class would be set
+		//Default data variables for the date
+		int year = 2023;
+		int month = 11;
+		int day = 25;
+		int hour = 11;
+		int duration = 2;
+		int minute = 0;
+
+		//Default variables for the time
+		//startTime = LocalDateTime.of(year, month, day, hour, 0);
+		startTime = LocalTime.of(hour, minute, 0);
+		ZoneId zone = ZoneId.systemDefault();
+		System.out.println("The event starts at " + startTime);
+		hour = hour + duration;
+		//endTime = LocalDateTime.of(year, month, day, hour+2, 0);
+		endTime = LocalTime.of(hour, minute, 0);
+		//These if statements below check if the time variables are too high
+		if(hour > 24) {
+			hour = hour - 24;
+			day++;
+			if(day > 31) {
+				day = day - 31;
+				month++;
+				if(month > 12) {
+					month = month - 12;
+					year++; //2023 would become 2024
+				}
+			}
+
+		}
+		System.out.println("The event ends at " + endTime);
+
+		plannedDate = LocalDate.of(year, month, day);
+		//date = Date.UTC(month, day, year);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+		System.out.println("Planned future date is " + plannedDate.format(formatter));
+
+		name = "UNT Football Game";
+		location = "1251 Bonnie Brae St. Denton, TX 76205";
+		scheduleType = "event";
+		System.out.println("The name of the " + scheduleType + " is " + name +
+				" and the location is at " + location);
 	}
 
 }
